87113f59eef1743c483a99394151f1374d6aa67e
Mehtab Alam <mehtaba728@gmail.com>
2024-01-16 21:14:45 +0500

    minor change in chapter 15

--- classes/04-javascript/class33/0-chapter-15/index.js
+++ classes/04-javascript/class33/0-chapter-15/index.js
@@ -1,19 +1,18 @@
 // CHAPTER-15: ARRAYS
 
-// problem: for multiple values we would have to create multiple variables/containers
+// Problem:
+// for multiple values we would have to create multiple variables/containers
 // const fruit0 = 'mango'
 // const fruit1 = 'banana'
 // const fruit2 = 'strawberry'
 
+// Solution:
 // to solve the above problem, we introduced the concept of arrays
 // so, array is a container that can store multiple values
 
 // create array
 // const fruits = [];
 // console.log(fruits);
-
-// concept
-// const hotel = ["Ali", "Bilal", "Choudhry", "Danyal", "Faisal"];
 
 // using emojis
 // const fruits = ["🍌", "🥭", "🍏", "🍊", "🍓", "🍐"];
@@ -29,12 +28,19 @@
 // console.log(fruits);
 
 // accessing specific array elements
-// const fruits = ["🍌", "🥭", "🍏", "🍊", "🍓", "🍐"];
+// concept of indices (singular: index)
+// const hotelRooms = ["Ali", "Bilal", "Choudhry", "Danyal", "Faisal"];
+// console.log(hotelRooms[4]);
+
 // const animals = ["🐱", "🐶", "🐄", "🐀", "🐅"];
-// console.log(animals[2]);
+// console.log(animals[0]);
 
 // mix different types
+// heterogeneous data vs homogeneous data
+// homogeneous: same type
+// heterogeneous: different types
 // const mixedData = [1, "hello", true, 3.5, "BYE"];
 // console.log(mixedData);
+// console.log(mixedData[1]);
 
 // arrays in js are dynamic
