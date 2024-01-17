d3425a9b5d62943b85faefa559c73f9466e93f16
Mehtab Alam <mehtaba728@gmail.com>
2024-01-17 09:36:59 +0500

    minor change in chapter-16

--- classes/04-javascript/class33/1-chapter-16/index.js
+++ classes/04-javascript/class33/1-chapter-16/index.js
@@ -1,18 +1,22 @@
 // CHAPTER-16 ADDING AND REMOVING ELEMENTS
 
 // const books = ["computer science", "mathematics"];
-
-// add element to the array
+// console.log(books);
+// add elements to the array
 
 // single
 // books.push("physics");
+// console.log(books);
 
 // multiple
 // books.push("physics", "chemistry");
+// console.log(books);
 
-// add element at a specifi position/index, it will replace already existing element
+// add element at a specific position/index, it will replace already existing element
+// const books = ["computer science", "mathematics"];
 // books[1] = "physics";
 // books[2] = "physics";
+// console.log(books);
 
 // adding elements to empty array
 // const books = [];
