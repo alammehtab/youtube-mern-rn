049c4e7635e7a9318f8bc88a55dc554f0738d1a1
Mehtab Alam <mehtaba728@gmail.com>
2024-01-30 10:02:12 +0500

    update chapter18 and 19

--- classes/04-javascript/class35/chapter-18/index.js
+++ classes/04-javascript/class35/chapter-18/index.js
@@ -23,21 +23,35 @@
 //  code block to be executed
 // }
 
-// Expression 1 is executed (one time) before the execution of the code block.
-// Expression 2 defines the condition for executing the code block.
-// Expression 3 is executed (every time) after the code block has been executed.
+// Expression 1,
+// 1. declare and variable and assign it a value
+// 2. it is executed (one time) before the execution of the code block.
+
+// Expression 2
+// 1. we check the condition
+// 2. defines the condition for executing the code block.
+
+// Expression 3
+// 1. increment/ decrement
+// 2. it is executed (every time) after the code block has been executed except the first time.
+
+// understanding for loop with a very simple example
+// print numbers 1 to 10
+// for (let number = 1; number <= 10; number++) {
+//   console.log(number);
+// }
 
 // verification of cleanest city using loops
-const cleanestCities = ["Peshawar", "Islamabad", "Karachi", "Lahore", "Multan"];
-const cityToCheck = prompt("Please enter your city name: ");
+// const cleanestCities = ["Peshawar", "Islamabad", "Karachi", "Lahore", "Multan"];
+// const cityToCheck = prompt("Please enter your city name: ");
 
-for (let cityIndex = 0; cityIndex < cleanestCities.length; cityIndex++) {
-  if (cityToCheck === cleanestCities[cityIndex]) {
-    alert("It's one of the cleanest cities");
-  }
-}
+// for (let cityIndex = 0; cityIndex < cleanestCities.length; cityIndex++) {
+//   if (cityToCheck === cleanestCities[cityIndex]) {
+//     alert("It's one of the cleanest cities");
+//   }
+// }
 
-// totalIterations: 5 (0,1,2,3,4)
+// totalIterations: 6 (0,1,2,3,4,5)
 // cityToCheck  = "Karachi"
 
 // iteration0:
@@ -64,3 +78,8 @@ for (let cityIndex = 0; cityIndex < cleanestCities.length; cityIndex++) {
 // cityIndex: 4
 // cityIndex < cleanestCities.length -> 4 < 5 (true)
 // cityToCheck === cleanestCities[cityIndex]-> "Karachi" === cleanestCities[4] -> "Karachi" === "Multan" -> FALSE
+
+// iteration5:
+// cityIndex: 5
+// cityIndex < cleanestCities.length -> 5 < 5 (false)
+// loop will be terminated because the condition is false
--- classes/04-javascript/class35/chapter-19/index.js
+++ classes/04-javascript/class35/chapter-19/index.js
@@ -1,12 +1,18 @@
 // CHAPTER-19: for loops:Flags, Booleans, array length, and loopus interruptus
 
-// flags:
+// flag:
+// Flag variable is used as a signal in programming to let the program know that a
+// certain condition has met. It usually acts as a boolean variable indicating a condition
+// to be either true or false
 
 // booleans
+// data type, 0s and 1s
 
 // array length
+// number of elements in an array
 
 // loopus interruptus
+// to break a loop before it's completion, meeting a condition
 
 // implementation
 const cleanestCities = ["Peshawar", "Islamabad", "Karachi", "Lahore", "Multan"];
@@ -18,22 +24,25 @@ let cityMili = false;
 for (let cityIndex = 0; cityIndex < cleanestCities.length; cityIndex++) {
   if (cityToCheck === cleanestCities[cityIndex]) {
     cityMili = true;
-    // alert("It's one of the cleanest cities");
-    // loopus intrrupt
+    // loopus intrruptus
     break;
+    // console.log("han g city mil gai");
   }
-  // console.log({ cityIndex });
+  // else {
+  //   console.log("city nahi mili");
+  // }
+  // console.log(cityIndex);
 }
-
-// using the flag
-// if (cityMili === false) {
-//   alert("It's not on the list");
-// } else {
-//   alert("It's one of the cleanest cities");
-// }
 
 if (cityMili) {
   alert("It's one of the cleanest cities");
 } else {
   alert("It's not on the list");
 }
+
+// using the flag (ye sb karne ki zaroorat nahi)
+// if (cityMili === true) {
+//   alert("It's one of the cleanest cities");
+// } else {
+//   alert("It's not on the list");
+// }
