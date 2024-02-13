5e1efec05a0cdb0feadeb2d98f105ebceec0df8e
Mehtab Alam <mehtaba728@gmail.com>
2024-02-13 18:44:54 +0500

    update chapter-24

--- classes/04-javascript/class39/chapter-24/index.js
+++ classes/04-javascript/class39/chapter-24/index.js
@@ -1,17 +1,31 @@
 // CHAPTER-24: Finding a character at a location
+
+// METHOD WE'LL DISCUSS
+// charAt
+
 // const institute = "saylani mass it training";
 // console.log(institute.charAt(14));
 
-// if u want to access the space
-// const mohalla = "saylani mass it training";
-// console.log(mohalla.charAt(7));
+// const fruit = "apple";
+// console.log(fruit.charAt(3));
 
-// example we've done already in last class
-const text = "hurrah!";
+// IF U WANT TO ACCESS SPACE
+// const greetings = "hi how are you?";
+// console.log(greetings.charAt(2));
 
-for (let i = 0; i < text.length; i++) {
-  if (text.charAt(i) === "!") {
-    console.log("Exclamation point found!");
+// EMAIL VALIDATION USING CHARAT
+const userEmail = prompt("Please enter your email address");
+let isValid = false;
+
+for (let charIndex = 0; charIndex < userEmail.length; charIndex++) {
+  if (userEmail.charAt(charIndex) === "@") {
+    isValid = true;
     break;
   }
+}
+
+if (isValid) {
+  console.log("Valid email address");
+} else {
+  console.log("Invalid email address");
 }
