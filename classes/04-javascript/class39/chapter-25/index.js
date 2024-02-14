a9d5bec5e5a53cc9c2edc72dbbfffea8f68acf0c
Mehtab Alam <mehtaba728@gmail.com>
2024-02-14 20:44:30 +0500

    update chapter-25

--- classes/04-javascript/class39/chapter-25/index.js
+++ classes/04-javascript/class39/chapter-25/index.js
@@ -1,12 +1,42 @@
+// Chapter-25: Strings: Replacing characters
+
+// method: replace
+
+// EXAMPLES
+// const fruit = "apple";
+// const modifiedVersion = fruit.replace("a", "A");
+// console.log(fruit);
+// console.log(modifiedVersion);
+
+// const exampleText = "ye hamara example text ha";
+// const replacedVersion = exampleText.replace("hamara", "apka");
+// console.log(exampleText);
+// console.log(replacedVersion);
+
+// const text = "sunaye kese hn ap?";
+// const afterReplace = text.replace("sunaye", "");
+// console.log(text);
+// console.log(afterReplace);
+
+// const text = "sunaye kese hn ap";
+// const afterReplace = text.replace("ap", "ap?");
+// console.log(text);
+// console.log(afterReplace);
+
 // let text = "hello, how are you doing?";
 // text = text.replace("doing", "doing buddy");
 // console.log(text);
 
-// let text = "excuse me, shut up!,i'm my own boss. shut up";
-// text = text.replace("shut up", "...");
-// console.log(text);
+// REAL-WORLD USE CASES
 
-// global replacement
-// let text = "excuse me, shut up!,i'm my own boss. shut up";
-// text = text.replace(/shut up/g, "...");
+// REPLACE SOMETHING ONCE
+// const text = "excuse me, shut up!,i'm my own boss. shut up";
+// const cleanText = text.replace("shut up", "...");
 // console.log(text);
+// console.log(cleanText);
+
+// REPLACE SOMETHING EVERYWHERE
+const text = "excuse me, shut up!,i'm my own boss. shut up";
+const cleanText = text.replace(/shut up/g, "...");
+console.log(text);
+console.log(cleanText);
