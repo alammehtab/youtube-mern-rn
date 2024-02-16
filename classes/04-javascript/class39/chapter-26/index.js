d448f6529137e5f5a78bc4d8074bd3d8f7654d88
Mehtab Alam <mehtaba728@gmail.com>
2024-02-16 08:46:17 +0500

    update chapter-26

--- classes/04-javascript/class39/chapter-26/index.js
+++ classes/04-javascript/class39/chapter-26/index.js
@@ -1,20 +1,59 @@
+// Chapter-26: Rounding Numbers
+
+// module we're gonna talk about
+// Math
+// console.log(Math.PI);
+
+// methods we're gonna talk about
+// round() -> it rounds a number to the nearest integer.
+// ceil() -> it rounds a number UP to the nearest integer.
+// floor() -> it rounds a number DOWN to the nearest integer.
+
 // const percentage = 73.56;
 // console.log(Math.round(percentage));
 
-// sawal
-// const percentage = 8.05;
-// console.log(Math.round(percentage));
+// ROUND
+// const a = Math.round(2.6);
+// console.log(a);
+// const b = Math.round(2.5);
+// console.log(b);
+// const c = Math.round(2.49);
+// console.log(c);
 
-// const number = -10.378;
-// console.log(math.round(number));
+// Negative numbers , such as -1, -2, -3, etc., are considered "DOWN" values and tend to round down. When rounding negative numbers, the rounding is done downwards, which means negative numbers are rounded down.
+// const d = Math.round(-2.6);
+// console.log(d);
+// const e = Math.round(-2.5);
+// console.log(e);
+// const f = Math.round(-2.49);
+// console.log(f);
 
-// const number = -10.8;
-// console.log(Math.round(number));
+// CEIL
+// console.log(Math.ceil(1.4));
+// const a = Math.ceil(0.6);
+// console.log(a);
+// const b = Math.ceil(0.4);
+// console.log(b);
+// const c = Math.ceil(5);
+// console.log(c);
+// const d = Math.ceil(5.1);
+// console.log(d);
+// const e = Math.ceil(-5.1);
+// console.log(e);
+// const f = Math.ceil(-5.9);
+// console.log(f);
 
-// ceil
-// const number = 7.2;
-// console.log(Math.ceil(number));
-
-// floor
-const number = 7.9;
-console.log(Math.floor(number));
+// FLOOR
+// console.log(Math.floor(1.6));
+// const a = Math.floor(0.6);
+// console.log(a);
+// const b = Math.floor(0.4);
+// console.log(b);
+// const c = Math.floor(5);
+// console.log(c);
+// const d = Math.floor(5.1);
+// console.log(d);
+// const e = Math.floor(-5.1);
+// console.log(e);
+// const f = Math.floor(-5.9);
+// console.log(f);
