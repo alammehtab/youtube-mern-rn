f321c82a98f2eaeeb7a452db9d6f430e57deaa0e
alammehtab <mehtaba728@gmail.com>
2024-03-27 23:31:19 +0500

    update class50

---
+++ classes/04-javascript/class50/chapter-37/index.js
@@ -0,0 +1,22 @@
+// Chapter-37: Functions: Passing data back from them
+
+// START KIA YAHA SE
+
+// function statement
+// function greet() {
+//   console.log("kese hn ap?");
+// }
+
+// function call
+// greet();
+// greet();
+
+// DATA PASS KIA, VERSATILE BANA DIA
+// function greet(msg) {
+//   console.log(msg);
+// }
+
+// greet("kese hn ap?");
+// greet("hello, how are you?");
+
+// FUNCTION SE DATA WAPS KESE LE?
