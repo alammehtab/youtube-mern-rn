ffc1ccc3fa7b84a72c5be400809257ed000d2674
alammehtab <mehtaba728@gmail.com>
2024-05-04 07:01:55 +0500

    finalize chapter40

--- classes/04-javascript/class51/chapter-40/index.js
+++ classes/04-javascript/class51/chapter-40/index.js
@@ -1,6 +1,37 @@
 // Chapter-40: switch statements: How to complete them
 
-// COMPLETE PROGRAM
-// program: which day of the week
+// RECAP OF THE LAST VIDEO
+// use of switch
+// switch vs if-else
+// example program
+// syntax- first 3 lines
 
-// COMPARISON BETWEEN IF STATEMENT AND SWITCH STATEMENT
+// UNDERSTANDING THE WHOLE PROGRAM
+const day = "Friday"
+
+switch (day) {
+    case "Monday":
+        console.log("Today is Monday")
+        break
+    case "Tuesday":
+        console.log("Today is Tuesday")
+        break
+    case "Wednesday":
+        console.log("Today is Wednesday")
+        break
+    case "Thurday":
+        console.log("Today is Thurday")
+        break
+    case "Friday":
+        console.log("Today is Friday")
+        console.log("BLESSED FRIDAY")
+        break
+    case "Saturday":
+        console.log("Today is Saturday")
+        break
+    case "Sunday":
+        console.log("Today is Sunday")
+        break
+    default:
+        console.log("Invalid day")
+}
