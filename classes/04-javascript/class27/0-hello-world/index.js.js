3581ce85daabdb5896830a6f227b191c993115c7
Mehtab Alam <49854015+alammehtab@users.noreply.github.com>
2024-05-18 10:20:05 +0500

    Update index.js.js

--- classes/04-javascript/class27/0-hello-world/index.js.js
+++ classes/04-javascript/class27/0-hello-world/index.js.js
@@ -1,4 +1,3 @@
-//PREVIOUS VIDEOS
 // we need language to communicate
 // what is a programming language?
 // machine language
@@ -8,10 +7,17 @@
 // decimal to binary conversion
 // what is javascript, ecma, ecmascript, node etc?
 
-//THIS VIDEO
+// hello world in browser console
+alert("Salam dunya")
+console.log("Salam dunya!");
+
+
 // hello world in js using vscode
+alert("Salam dunya")
 console.log("Hello world!");
 
-// hello world in js using browser console
 // hello world in js using replit
 // hello world in js using programiz JS IDE
+
+// types in javascript
+https://www.w3schools.com/js/js_datatypes.asp
