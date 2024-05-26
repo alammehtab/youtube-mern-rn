42117415680006caa948bb7c2faaf7ce5c3ba18b
alammehtab <mehtaba728@gmail.com>
2024-05-26 12:12:55 +0500

    update chapter-5

--- classes/04-javascript/class29/0-chapter-5/index.js
+++ classes/04-javascript/class29/0-chapter-5/index.js
@@ -5,102 +5,110 @@
 // + (addition) if all the operands are Numbers
 // 2 + 3
 // in above lin 2 and 3 are operands and + is an operator
-// const firstNumber = 2;
-// const secondNumber = 3;
-// //  result will be 5
-// const result = firstNumber + secondNumber;
-// console.log(result);
+// var firstNumber = 2;
+// var secondNumber = 31;
+// var result = firstNumber + secondNumber;
+// console.log(result)
 
 // + concatenation if any one of the operands is a string
-// result will be 217
-// const result = 2 + "17";
+// var result = 2 + "17";
 // console.log(result);
 
-// result 7122
-// const result = "71" + 2 + 2;
+// var result = "71" + 2 + 2;
 // console.log(result);
 
-// result 2272
-// const result = 2 + "27" + 2;
+// var result = 2 + "27" + 2;
 // console.log(result);
 
-// result will be 557
-// const result = 3 + 2 + "57";
-// console.log(result);
+// var result = 3 + 2 + "57";
+// console.log(result)
 
-// result will be 69
-// const result = 2 + 4 + "7" * 9;
-// console.log(result);
-// BODMAS
-// brackets, order, division, multiplication, addition, subtraction
 
 // - if any operand is a valid number, minus
-// const firstNumber = 88;
-// const secondNumber = 3;
+// var firstNumber = 18;
+// var secondNumber = 8;
+// var result = firstNumber - secondNumber;
+// console.log({ result });
 
-// result will be 85
-// const result = firstNumber - secondNumber;
+
+// var result = "15" - 6;
 // console.log(result);
 
-// result is 9
-// const result = "15" - 6;
+// var result = 15 - "6";
 // console.log(result);
+
+// var result = "15" - "6";
+// console.log(result);
+
+// var result = "15" - "6" - 2;
+// console.log(result);
+
+// var result = "15" - "6" - "2";
+// console.log(result);
+
+// var result = "b" - 2;
+// console.log(result);
+
 
 // *
-// const firstNumber = 2;
-// const secondNumber = 5;
-
-// result is 10
-// const result = firstNumber * secondNumber;
+// var firstNumber = 2;
+// var secondNumber = 10;
+// var result = firstNumber * secondNumber;
 // console.log(result);
 
-// result 24
-// const result = "8" * "3";
+// var result = "4" * "2";
 // console.log(result);
 
-// result is 10
-// const result = 5 * "2";
+
+// var result = 5 * "12";
 // console.log(result);
+
+
+// var result = 5 * "v";
+// console.log(result);
+
 
 // Division /
 // returns quotient
-// const dividend = 15;
-// const divisor = 4;
+// var dividend = 17;
+// var divisor = 4;
 
-// result is 3.75  bcz it divides exactly
-// const result = dividend / divisor;
+// var result = dividend / divisor;
 // console.log(result);
 
-// result is 5
-// const result = 15 / "3";
+// var result = 7 / "3";
 // console.log(result);
 
-// result is 5
-// const result = "15" / "3";
+
+// var result = "15" / "3";
 // console.log(result);
 
 // modulus operator % return remainder
-// const dividend = 15;
-// const divisor = 2;
+// var dividend = 15;
+// var divisor = 2;
+// var result = dividend % divisor;
+// console.log(result)
 
-// result is 1 because 15 divided by 2 is 7 and 1 remains
-// const result = dividend % divisor;
+// var dividend = 14;
+// var divisor = 2;
+// var result = dividend % divisor;
+// console.log(result)
 
-// const dividend = 14;
-// const divisor = 2;
 
-//  result is  because 14 divided by  is 7 and  remains
-// const result = dividend % divisor;
+// var result = 0 % 1;
+// console.log(result)
 
-// result 0
-// const result = 0 % 1;
+// var result = 2 % 5;
+// console.log(result)
 
-// result NaN  bcz of division by zero
-// const result = 1 % 0;
+
+// var result = 1 % 0;
+// console.log(result)
 
 // printing better messages
-// const firstNumber = 4;
-// const secondNumber = 5;
-// const sum = firstNumber + secondNumber;
+// var firstNumber = 4;
+// var secondNumber = 5;
+// var sum = firstNumber + secondNumber;
 
 // console.log(`The sum of ${firstNumber} and ${secondNumber} is ${sum}!`);
+// console.log("The sum of " + firstNumber + " and " + secondNumber + " is " + sum + "!")
