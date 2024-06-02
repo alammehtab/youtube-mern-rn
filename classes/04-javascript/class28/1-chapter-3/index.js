42f6c7d20896096b5542e0235ac354e2de60e9a5
alammehtab <mehtaba728@gmail.com>
2024-06-02 10:20:55 +0500

    updater chapter3

--- classes/04-javascript/class28/1-chapter-3/index.js
+++ classes/04-javascript/class28/1-chapter-3/index.js
@@ -1,48 +1,36 @@
 // CHAPTER-3 VARIABLES FOR NUMBERS
 
 // we can store numbers in variables
-// const name = "cs with alam";
+// var name = "cs with alam";
 // console.log(name);
-// const mySalary = 200;
-// const myHeight = 5.9;
+// var mySalary = 200;
+// var myHeight = 5.9;
 // console.log(myHeight);
 
 // a variable can be used to calculate its own new value
-// let chocolatesBox = 70;
+// var chocolatesBox = 70;
 // chocolatesBox = chocolatesBox + 20;
 // console.log(chocolatesBox);
 
 // can store the result in new variable
-// let chocolatesBox = 15;
-// let newBox = chocolatesBox + 5;
+// var chocolatesBox = 15;
+// var newBox = chocolatesBox + 5;
 // console.log(newBox);
 
-// addition vs concatenation
+// ADDITION VS CONCATENATION
 
 // can add two variables
-// const firstNumber = 1;
-// const secondNumber = 2;
+// var firstNumber = 1;
+// var secondNumber = 2;
 // console.log(firstNumber + secondNumber);
 
 // number enclosed in quotes
-// const firstNumber = 1;
-// const secondNumber = "2";
+// var firstNumber = 1;
+// var secondNumber = "2";
 // console.log(firstNumber + secondNumber);
 
 // concatenation: making full name
-// const firstName = "Muhammad";
-// const lastName = "Bilal";
-// const fullName = firstName + " " + lastName;
+// var firstName = "Muhammad";
+// var lastName = "Bilal";
+// var fullName = firstName + " " + lastName;
 // console.log(fullName);
-
-// variable name can't start with a number, can use number inside or at the end
-
-// // illegal
-// const 1position = "ali";
-
-// //legal
-// const position1 = "ali";
-// console.log(position1);
-
-const hello123Bye = "Hello and goodbye!";
-console.log(hello123Bye);
