5b9a5e168a4187e7e80a90978e8f4d366bab6c83
alammehtab <mehtaba728@gmail.com>
2024-06-02 10:25:59 +0500

    update chapter4

--- classes/04-javascript/class28/2-chapter-4/index.js
+++ classes/04-javascript/class28/2-chapter-4/index.js
@@ -1,49 +1,46 @@
 // CHAPTER-4 VARIABLE NAMES LEGAL AND ILLEGAL
 
-// A variable name can't contain any spaces.
+// A VARIABLE NAME CAN'T CONTAIN ANY SPACES.
 // For example, my name, full name
-// const my name = 'mehtab'//illegal
-// const myName = "mehtab";
-// const myFirstName = 'mehtab';
+// var my name = 'mehtab'//illegal
+// var myName = "mehtab";
+// var myFirstName = 'mehtab';
 
-// A variable name can contain only letters, numbers, dollar signs, and underscores.
+// A VARIABLE NAME CAN CONTAIN ONLY LETTERS, NUMBERS, DOLLAR SIGNS, AND UNDERSCORES.
 // For example, myName, myName1, my1Name, student10Name, my$name, my_name
-// const myName ="mehtab"
-// const myName1 = 'mehtab'
-// const my1Name = 'Mehtab';
-// const $myName = 'Mehtab'
-// const my$name = 'mehtab'
-// const  my_name = 'Mehtab';
-// const _name = 'mehtab'
-// const my_first_name = "Mehtab";
+// var myName ="mehtab"
+// var myName1 = 'mehtab'
+// var my1Name = 'Mehtab';
+// var $myName = 'Mehtab'
+// var my$name = 'mehtab'
+// var  my_name = 'Mehtab';
+// var _name = 'mehtab'
+// var my_first_name = "Mehtab";
 
-// Though a variable name can't be any of JavaScript's keywords, it can contain keywords.
+// THOUGH A VARIABLE NAME CAN'T BE ANY OF JAVASCRIPT'S KEYWORDS, IT CAN CONTAIN KEYWORDS.
 // For example, userAlert and myVar are legal.
-// const function //// it's illegal
-// const myFunction = "my function"; ////legal
+// var function //// it's illegal
+// var myFunction = "my function"; ////legal
 // console.log(myFunction);
-// const const = 'my const' ////illegal
+// var var = 'my var' ////illegal
 
-// Capital letters are fine, but be careful. Variable names are case sensitive.
+// CAPITAL LETTERS ARE FINE, BUT BE CAREFUL. VARIABLE NAMES ARE CASE SENSITIVE.
 // For example, A bike is not a Bike 🚲.
-// const bike = "super bike";
+// var bike = "super bike";
 // console.log(bike); //// output: super bike
 // console.log(Bike); ////error: Bike not defined
-// const bIKE = "another bike";
+// var bIKE = "another bike";
 // console.log(bike); //// output: super bike
 // console.log(bIKE); //// output: another bike
 
-// I teach the camelCase 🐪 naming convention. Why "camelCase"? Because there is a hump or
-// two (or three) in the middle if the name is formed by more than one word. A camelCase
-// name begins in lower case. If there's more than one word in the name, each subsequent
-// word gets an initial cap, creating a hump.
+// I TEACH THE CAMELCASE 🐪 NAMING CONVENTION.WHY CAMELCASE ? BECAUSE THERE IS A HUMP OR TWO(OR THREE) IN THE MIDDLE IF THE NAME IS FORMED BY MORE THAN ONE WORD.A CAMELCASE NAME BEGINS IN LOWER CASE.IF THERE'S MORE THAN ONE WORD IN THE NAME, EACH SUBSEQUENT WORD GETS AN INITIAL CAP, CREATING A HUMP.
 // For example, name, firstName, myFirstName
 
-// const name = "mehtab";
-// const firstName = "mehtab";
-// const myFirstName = "Mehtab";
+// var name = "mehtab";
+// var firstName = "mehtab";
+// var myFirstName = "Mehtab";
 
 // snack case
-const name = "mehtab";
-const first_name = "mehtab";
-const my_first_name = "Mehtab";
+// var name = "mehtab";
+// var first_name = "mehtab";
+// var my_first_name = "Mehtab";
