a632ecbe45fe3915d3ac2f0f42221d596452cf8b
alammehtab <mehtaba728@gmail.com>
2024-06-02 10:18:22 +0500

    update chapter2

--- classes/04-javascript/class28/0-chapter-2/index.js
+++ classes/04-javascript/class28/0-chapter-2/index.js
@@ -4,39 +4,13 @@
 // how to create a variable?
 // var gradeFiveBooks = "my grade 5 books";
 // what is es6 or es2015?
-// const gradeFiveBooks = "my grade 5 books";
 // what is a block?
 
-// difference b/w var,let, and const?
-
-// var vs let
-// function hello() {
-//   {
-//     let secret = "my dark secret";
-//     // alert(secret);
-//   }
-
-//   {
-//     alert("bahir se secret access hogya: " + secret);
-//   }
-// }
-// hello();
-
-// let vs const
-
-// const PI = 3.14;
-// PI = 6.28;
-
-// let height = 2.7;
-// height = 2.9;
-
-// console.log(height);
-
 // assignment operator
-// const gradeFiveBooks = "my grade 5 books";
+// var gradeFiveBooks = "my grade 5 books";
 
 // advantage of using variables, one of them is: change value at one place and it will reflect everywhere
-// const myAge = 35;
+// var myAge = 35;
 // console.log(myAge);
 // console.log(myAge);
 // console.log(myAge);
@@ -53,14 +27,6 @@
 // console.log(myAge);
 // console.log(myAge);
 // console.log(myAge);
-
-// difference b/w = , ==, and ===
-// == checks just value, === checks value and type as well
-// if (1 === "1") {
-//   console.log("han g barabar ha");
-// } else {
-//   console.log("nahi g nahi barabar");
-// }
 
 // declaration vs assignment
 
@@ -68,6 +34,7 @@
 // let age = 25;
 
 // declaration-> container banana
-// let age;
+// var age;
+
 // assignment-> container k andar kuch rakhna/dalna
 // age = 30;
