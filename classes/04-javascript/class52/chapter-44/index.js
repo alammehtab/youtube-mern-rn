391be11ef3f73ba9a9f5acf41e2f1f744fa7f26a
alammehtab <mehtaba728@gmail.com>
2024-06-03 16:31:39 +0500

    update chapter44

--- classes/04-javascript/class52/chapter-44/index.js
+++ classes/04-javascript/class52/chapter-44/index.js
@@ -1,9 +1,20 @@
 // Chapter-44: Commenting
 
-// WHAT ARE COMMENTS?
+// JAVASCRIPT COMMENTS
 
-// WHY COMMENTS?
+// JavaScript comments can be used to explain JavaScript code, and to make it more readable.
+// JavaScript comments can also be used to prevent execution, when testing alternative code.
 
 // SINGLE LINE COMMENTS
 
+// Single line comments start with //.
+// Any text between // and the end of the line will be ignored by JavaScript (will not be executed).
+
 // MULTI LINE COMMENTS
+
+// Multi - line comments start with /* and end with */.
+// Any text between /* and */ will be ignored by JavaScript.
+
+// USING COMMENTS TO PREVENT EXECUTION
+// Using comments to prevent execution of code is suitable for code testing.
+// Adding // in front of a code line changes the code lines from an executable line to a comment.
