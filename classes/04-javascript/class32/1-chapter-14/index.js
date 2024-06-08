ed1908128a8cc1cc01cb0b0254eb01d9412e728f
alammehtab <mehtaba728@gmail.com>
2024-06-08 18:22:10 +0500

    update chapter14

--- classes/04-javascript/class32/1-chapter-14/index.js
+++ classes/04-javascript/class32/1-chapter-14/index.js
@@ -23,15 +23,31 @@
 // }
 
 // Ticket Pricing
-const age = +prompt("Please enter your age");
-const isStudent = +prompt(`Are you a student\n0.No\n1.Yes`);
+// const age = +prompt("Please enter your age");
+// const isStudent = +prompt(`Are you a student\n0.No\n1.Yes`);
 
-if (age < 12) {
-  document.write("Child Ticket: $5");
-} else {
-  if (age >= 12 && age < 18 && isStudent) {
-    document.write("Student Ticket: $10");
-  } else {
-    document.write("Adult Ticket: $15");
+// if (age < 12) {
+//   document.write("Child Ticket: $5");
+// } else {
+//   if (age >= 12 && age < 18 && isStudent) {
+//     document.write("Student Ticket: $10");
+//   } else {
+//     document.write("Adult Ticket: $15");
+//   }
+// }
+
+// SARKARI NOKRI
+// REQUIREMENTS:
+// AGE: 18-25
+// GENDER: male
+
+var age = 19
+var gender = 'female'
+
+if (age >= 18 && age <= 25) {
+  if (gender === 'male') {
+    console.log(`Eligible`)
   }
+} else {
+  console.log(`Not Eligible`)
 }
