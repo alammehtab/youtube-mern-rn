ea28e896a5743b968aaa4017474bffa7154fce37
alammehtab <mehtaba728@gmail.com>
2024-06-29 08:00:07 +0500

    update chapter44

--- classes/04-javascript/class52/chapter-44/index.js
+++ classes/04-javascript/class52/chapter-44/index.js
@@ -4,16 +4,30 @@
 
 // JavaScript comments can be used to explain JavaScript code, and to make it more readable.
 // JavaScript comments can also be used to prevent execution, when testing alternative code.
+// console.log(10 + 20)
+// console.log(5 + 7)
 
-// SINGLE LINE COMMENTS
+// SINGLE LINE COMMENTS (keyboard shortcut: ctrl+/)
 
 // Single line comments start with //.
 // Any text between // and the end of the line will be ignored by JavaScript (will not be executed).
+
+// EXAMPLE
+// var animals = []; // Declare an empty array
 
 // MULTI LINE COMMENTS
 
 // Multi - line comments start with /* and end with */.
 // Any text between /* and */ will be ignored by JavaScript.
+
+// EXAMPLE
+/* The following function named add
+    is a function that adds two numbers and
+    returns the result.
+*/
+// function add(a, b) {
+//     return a + b;
+// }
 
 // USING COMMENTS TO PREVENT EXECUTION
 // Using comments to prevent execution of code is suitable for code testing.
