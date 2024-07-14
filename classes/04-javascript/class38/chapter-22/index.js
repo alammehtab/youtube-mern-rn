f6362276703ee9dfd8faae156dfebabc8bdefca1
alammehtab <mehtaba728@gmail.com>
2024-07-14 10:46:39 +0500

    update chapter22

--- classes/04-javascript/class38/chapter-22/index.js
+++ classes/04-javascript/class38/chapter-22/index.js
@@ -1,39 +1,37 @@
 // CHAPTER-22: Strings:Measuring length and extracting parts
 
 // FINDING LENGTH OF STRING
-// const channelName = "cs with alam";
-// const channelNameLength = channelName.length;
-// console.log(channelNameLength);
 // console.log("first".length);
+// var channelName = "cs with alam";
+// var channelNameLength = channelName.length;
+// console.log(channelNameLength);
 
 // EXTRACT PART OF A STRING
 // array recap
-// const numbers = [1, 2, 3, 4, 5];
-// const arrayPart = numbers.slice(0, 2);
+// var numbers = [1, 2, 3, 4, 5];
+// var arrayPart = numbers.slice(0, 2);
 // console.log(arrayPart);
 
 // slice with strings
-// let language = "JavaScript";
+var language = "JavaScript";
 
 // using another variable
-// const sliced = language.slice(1, 3);
+// var sliced = language.slice(1, 3);
 // console.log(sliced);
 
 // not using another variable
-// language = language.slice(1, 5);
-// console.log(language);
 // console.log(language.slice(0, 4));
+// console.log(language.slice(1, 5))
 
 // USER INPUT CITY NAME, FIRST LETTER CAPITAL
 
-// const city = prompt("Please enter your city name");
-// const firstLetter = city[0];
-// const firstLetter = city.slice(0, 1);
-// const capitalFirstLetter = firstLetter.toUpperCase();
-// const remainingLetters = city.slice(1);
-// console.log(firstLetter);
-// console.log(remainingLetters);
-// const finalResult = capitalFirstLetter + remainingLetters;
+// var city = prompt("Please enter your city name");
+// var firstLetter = city[0];
+// var firstLetter = city.slice(0, 1);
+// var capitalFirstLetter = firstLetter.toUpperCase();
+// var remainingLetters = city.slice(1);
+// console.log({ firstLetter, capitalFirstLetter, remainingLetters })
+// var finalResult = capitalFirstLetter + remainingLetters;
 // console.log(finalResult);
 
 // MONTH NAME ABBREVIATION IF LENGTH > 3
@@ -41,17 +39,17 @@
 // January -> Jan
 // May -> May
 
-const monthName = prompt("Please enter a month name");
+// var monthName = prompt("Please enter a month name");
 
-if (monthName.length > 3) {
-  const shortForm = monthName.slice(0, 3);
-  const firstLetter = shortForm.slice(0, 1).toUpperCase();
-  const remainingLetters = shortForm.slice(1);
-  const finalResult = firstLetter + remainingLetters;
-  console.log(finalResult);
-} else {
-  const firstLetter = monthName.slice(0, 1).toUpperCase();
-  const remainingLetters = monthName.slice(1);
-  const finalResult = firstLetter + remainingLetters;
-  console.log(finalResult);
-}
+// if (monthName.length > 3) {
+//     var firstThreeCharacters = monthName.slice(0, 3);
+//     var firstCharacter = firstThreeCharacters.slice(0, 1).toUpperCase();
+//     var remainingCharacters = firstThreeCharacters.slice(1);
+//     var finalResult = firstCharacter + remainingCharacters;
+//     console.log(finalResult);
+// } else {
+//     var firstCharacter = monthName.slice(0, 1).toUpperCase();
+//     var remainingCharacters = monthName.slice(1);
+//     var finalResult = firstCharacter + remainingCharacters;
+//     console.log(finalResult);
+// }
