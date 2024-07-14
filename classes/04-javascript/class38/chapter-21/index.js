68c644b786c09b0250a90a3e1e474cbb6f1274e6
alammehtab <mehtaba728@gmail.com>
2024-07-14 10:06:20 +0500

    chapter class21

--- classes/04-javascript/class38/chapter-21/index.js
+++ classes/04-javascript/class38/chapter-21/index.js
@@ -6,70 +6,69 @@
 // HELLO WORLD
 
 // toLowerCase()
-// const city = "peshawar";
-// console.log(city);
-
-// using second variable
-// let city = "PESHAWAR";
-// const smallCity = city.toLowerCase();
-// console.log(city);
-// console.log(smallCity);
 
 // without using second variable
+// method:1
+// var city = "PESHAWAR";
 // console.log(city.toLowerCase());
+
+// method:2
 // city = city.toLowerCase();
 // console.log(city);
 
+// using second variable
+// var city = "PESHAWAR";
+// var smallCity = city.toLowerCase();
+// console.log(city);
+// console.log(smallCity);
+
+
 // zaroori nhi ha k all uppercase ho, mix b hosakte hn
-// const city = "KaRachi";
+// var city = "KaRachi";
 // console.log(city.toLowerCase());
 
 // toUpperCase()
-// let city = "karachi";
-
-// using second variable
-// const capitalizedCity = city.toUpperCase();
-// console.log(city);
-// console.log(capitalizedCity);
+// var city = "karachi";
 
 // without using second variable
 // console.log(city.toUpperCase());
 // city = city.toUpperCase();
 // console.log(city);
 
+// using second variable
+// var capitalizedCity = city.toUpperCase();
+// console.log(city);
+// console.log(capitalizedCity);
+
+
 // zaroori nhi ha k all lowercase ho, mix b hosakte hn
-// const city = "peShaWar";
+// var city = "peShaWar";
 // console.log(city.toUpperCase());
 
 // REAL WORLD EXAMPLE
 // latptop scheme
-const eligibleUniversities = [
-  "sarhad",
-  "iqra",
-  "islamia",
-  "city",
-  "ims",
-  "cecos",
-  "nust",
-  "agriculture",
-];
-const studentUniversity = prompt("Please enter your university name");
-let isEligible = false;
+// var eligibleUniversities = [
+//     "sarhad",
+//     "iqra",
+//     "islamia",
+//     "city",
+//     "ims",
+//     "cecos",
+//     "nust",
+//     "agriculture",
+// ];
+// var studentUniversity = prompt("Please enter your university name");
+// var isEligible = false;
 
-for (
-  let universityIndex = 0;
-  universityIndex < eligibleUniversities.length;
-  universityIndex++
-) {
-  if (
-    studentUniversity.toLowerCase() === eligibleUniversities[universityIndex]
-  ) {
-    isEligible = true;
-    console.log("Eligible");
-    break;
-  }
-}
+// for (var i = 0; i < eligibleUniversities.length; i++) {
+//     if (studentUniversity.toLowerCase() === eligibleUniversities[i]) {
+//         isEligible = true;
+//         break;
+//     }
+// }
 
-if (!isEligible) {
-  console.log("Not Eligible");
-}
+// if (isEligible) {
+//     alert("Eligible!");
+// } else {
+//     alert("Not Eligible!")
+// }
