286fcbad0ac197f7e8fa3188e91cc5bb3ce75f80
alammehtab <mehtaba728@gmail.com>
2024-07-20 10:22:31 +0500

    update chapter24

--- classes/04-javascript/class39/chapter-24/index.js
+++ classes/04-javascript/class39/chapter-24/index.js
@@ -3,29 +3,33 @@
 // METHOD WE'LL DISCUSS
 // charAt
 
-// const institute = "saylani mass it training";
+// var institute = "saylani mass it training";
 // console.log(institute.charAt(14));
 
-// const fruit = "apple";
+// CAN'T USE WITH ARRAYS
+// var mohalla = ["ali", "ahmad", "akram", "aslam", "ajmal", "aslam"];
+// alert(mohalla.charAt(3));
+
+// var fruit = "apple";
 // console.log(fruit.charAt(3));
 
 // IF U WANT TO ACCESS SPACE
-// const greetings = "hi how are you?";
+// var greetings = "hi how are you?";
 // console.log(greetings.charAt(2));
 
 // EMAIL VALIDATION USING CHARAT
-const userEmail = prompt("Please enter your email address");
-let isValid = false;
+// var userEmail = prompt("Please enter your email address");
+// var isValid = false;
 
-for (let charIndex = 0; charIndex < userEmail.length; charIndex++) {
-  if (userEmail.charAt(charIndex) === "@") {
-    isValid = true;
-    break;
-  }
-}
+// for (var charIndex = 0; charIndex < userEmail.length; charIndex++) {
+//     if (userEmail.charAt(charIndex) === "@") {
+//         isValid = true;
+//         break;
+//     }
+// }
 
-if (isValid) {
-  console.log("Valid email address");
-} else {
-  console.log("Invalid email address");
-}
+// if (isValid) {
+//     alert("Valid email address");
+// } else {
+//     alert("Invalid email address");
+// }
