c9a0618ff29c3dd098487c83a827f1249c26043e
alammehtab <mehtaba728@gmail.com>
2024-07-20 10:14:14 +0500

    update chapter23

--- classes/04-javascript/class38/chapter-23/index.js
+++ classes/04-javascript/class38/chapter-23/index.js
@@ -2,60 +2,62 @@
 
 // methods we're gonna discuss
 // indexOf
-// const book = "Think and Grow Rich";
-// const indexOfK = book.indexOf("k");
+// var book = "Think and Grow Rich";
+// var indexOfK = book.indexOf("k");
 // console.log(indexOfK);
 
 // ALWAYS RETURNS THE INDEX OF VERY FIRST MATCH
-// const greetings = "hello";
+// var greetings = "hello";
 // console.log(greetings.indexOf("l"));
 
 // CAN PASS STRING AS WELL
-// const book = "Think and Grow Rich";
+// var book = "Think and Grow Rich";
 // console.log(book.indexOf("and Grow"));
-
-// APKO WO INDEX DETA HA JISPE MATCH MILE
-// const email = "test@test.com";
-// console.log(email.indexOf("r"));
-// console.log(email.indexOf("@gmail.com"));
 
 // APKO -1 DETA HA AGAR MATCH NA MILE
 // console.log(email.indexOf("r"));
 
+// APKO WO INDEX DETA HA JISPE MATCH MILE
+// var email = "test@test.com";
+// console.log(email.indexOf("r"));
+// console.log(email.indexOf("@gmail.com"));
+
 // WE CAN USE SIMPLE ARRAYS AS WELL
-// const mohalla = ["ali", "ahmad", "akram", "aslam", "ajmal", "aslam"];
+// var mohalla = ["ali", "ahmad", "akram", "aslam", "ajmal", "aslam"];
 // console.log(mohalla.indexOf("aslam"));
-// console.log(mohalla.lastIndexOf("aslam"));
 
 // lastIndexOf
 
 // as the name suggests, it always returns the index of last match
-// const greetings = "hello";
+// var greetings = "hello";
 // console.log(greetings.lastIndexOf("l"));
 
-// EMAIL VALIDATION WITHOUT USING INDEXOF
-// const userEmail = prompt("Please enter your email address");
-// let isValid = false;
+// var mohalla = ["ali", "ahmad", "akram", "aslam", "ajmal", "aslam"];
+// console.log(mohalla.lastIndexOf("aslam"));
 
-// for (let charIndex = 0; charIndex < userEmail.length; charIndex++) {
-//   if (userEmail[charIndex] === "@") {
-//     isValid = true;
-//     break;
-//   }
+// EMAIL VALIDATION WITHOUT USING INDEXOF (must include @)
+// var userEmail = prompt("Please enter your email address");
+// var isValid = false;
+
+// for (var charIndex = 0; charIndex < userEmail.length; charIndex++) {
+//     if (userEmail[charIndex] === "@") {
+//         isValid = true;
+//         break;
+//     }
 // }
 
 // if (isValid) {
-//   console.log("Valid email address");
+//     alert("Valid email address");
 // } else {
-//   console.log("Invalid email address");
+//     alert("Invalid email address");
 // }
 
 // EMAIL VALIDATION USING INDEXOF
-// const email = prompt("Please enter your email");
-// const isValid = email.indexOf("@");
+// var email = prompt("Please enter your email");
+// var isValid = email.indexOf("@");
 
 // if (isValid === -1) {
-//   console.log("Invalid email");
+//     alert("Invalid email");
 // } else {
-//   console.log("Valid email");
+//     alert("Valid email");
 // }
