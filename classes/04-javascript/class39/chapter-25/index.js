aca73c644f4d511b0bd6c0ada0a8c691b15b2516
alammehtab <mehtaba728@gmail.com>
2024-07-20 10:34:56 +0500

    update chapter25

--- classes/04-javascript/class39/chapter-25/index.js
+++ classes/04-javascript/class39/chapter-25/index.js
@@ -3,40 +3,34 @@
 // method: replace
 
 // EXAMPLES
-// const fruit = "apple";
-// const modifiedVersion = fruit.replace("a", "A");
-// console.log(fruit);
-// console.log(modifiedVersion);
+// var fruit = "apple";
+// var modifiedVersion = fruit.replace("a", "A");
+// console.table([fruit, modifiedVersion]);
 
-// const exampleText = "ye hamara example text ha";
-// const replacedVersion = exampleText.replace("hamara", "apka");
-// console.log(exampleText);
-// console.log(replacedVersion);
+// var exampleText = "ye hamara example text ha";
+// var replacedVersion = exampleText.replace("hamara", "apka");
+// console.table([exampleText, replacedVersion]);
 
-// const text = "sunaye kese hn ap?";
-// const afterReplace = text.replace("sunaye", "");
-// console.log(text);
-// console.log(afterReplace);
+// var text = "sunaye kese hn ap?";
+// var afterReplace = text.replace("sunaye", "");
+// console.table([text, afterReplace]);
 
-// const text = "sunaye kese hn ap";
-// const afterReplace = text.replace("ap", "ap?");
-// console.log(text);
-// console.log(afterReplace);
+// var text = "sunaye kese hn ap";
+// var afterReplace = text.replace("ap", "ap?");
+// console.table([text, afterReplace]);
 
-// let text = "hello, how are you doing?";
+// var text = "hello, how are you doing?";
 // text = text.replace("doing", "doing buddy");
 // console.log(text);
 
 // REAL-WORLD USE CASES
 
 // REPLACE SOMETHING ONCE
-// const text = "excuse me, shut up!,i'm my own boss. shut up";
-// const cleanText = text.replace("shut up", "...");
-// console.log(text);
-// console.log(cleanText);
+// var text = "excuse me, shut up!,i'm my own boss. shut up";
+// var cleanText = text.replace("shut up", "...");
+// console.table([text, cleanText]);
 
 // REPLACE SOMETHING EVERYWHERE
-const text = "excuse me, shut up!,i'm my own boss. shut up";
-const cleanText = text.replace(/shut up/g, "...");
-console.log(text);
-console.log(cleanText);
+// var text = "excuse me, shut up!,i'm my own boss. shut up";
+// var cleanText = text.replace(/shut up/g, "...");
+// console.table([text, cleanText]);
