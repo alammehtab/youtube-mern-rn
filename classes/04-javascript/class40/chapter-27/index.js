4a270067b9dd3b1f87a794890e04e47e7aa775d5
alammehtab <mehtaba728@gmail.com>
2024-07-21 10:58:05 +0500

    update chapter27

--- classes/04-javascript/class40/chapter-27/index.js
+++ classes/04-javascript/class40/chapter-27/index.js
@@ -7,49 +7,43 @@
 // 3. always delivers a 16-place decimal that ranges from 0.0000000000000000 to 0.9999999999999999
 
 // GENERATING A RANDOM NUMBER
-// const randomNumber = Math.random();
+// var randomNumber = Math.random();
 // console.log(randomNumber);
 
 // CONVERTING A RANDOM NUMBER TO AN INTEGER
 // There is no such thing as JavaScript integers.
 // We are talking about numbers with no decimals here.
 
-// removing the decimal point and getting the whole number as an integer
-// const randomDecimal = Math.random();
-// const randomInteger = randomDecimal * 10000000000000000;
-// console.log(randomDecimal);
-// console.log(randomInteger);
-
 // getting just the integer part, removing the decimal part (numbers after decimal point)
-// let randomNumber = Math.random();
-// const intergerNumber = Math.floor(randomNumber);
+// var randomNumber = Math.random();
+// var intergerNumber = Math.floor(randomNumber);
 // console.log(intergerNumber);
 
 // GENERATING A RANDOM NUMBER FROM 0.0000000000000000 to 5.9999999999999999
-// let randomNumber = Math.random();
+// var randomNumber = Math.random();
 // randomNumber = randomNumber * 6; // upper limit exclusive hoti ha
 // console.log({ randomNumber });
 
 // GENERATING RANDOM NUMBER FROM 0 TO N (0 inclusive, n exlclusive)
-// const n = 10;
-// const randomNumber = Math.random() * n;
-// const randomInteger = Math.floor(randomNumber);
+// in this case 0-9
+// var n = 10;
+// var randomNumber = Math.random() * n;
+// var randomInteger = Math.ceil(randomNumber);
 // console.log(randomInteger);
 
-// LUDO DICE EXAMPLE, RANDOM NUMBERS 1 TO 6
-// let bigDecimal = Math.random();
-// let improvedNum = bigDecimal * 6 + 1; // 1.something - 6.something
-// let numberOnDice = Math.floor(improvedNum);
+// LUDO DICE EXAMPLE, RANDOM NUMBERS 1 TO 6 (method0: +1)
+// var decimalInRange = Math.random() * 6 + 1; // 1.something - 6.something
+// var numberOnDice = Math.floor(decimalInRange); // 1 to 6
 
-// console.log({ bigDecimal, improvedNum, numberOnDice });
+// console.table([bigDecimal, numberOnDice ]);
 
-// GUESS THE NUMBER, GAME, 1-3
-const randomNumber = Math.ceil(Math.random() * 3);
-const userGuess = +prompt("Guess the number between 1 and 3");
+// GUESS THE NUMBER, GAME, 1-3 (method1: ceil)
+// var randomNumber = Math.ceil(Math.random() * 3);
+// var userGuess = +prompt("Guess the number between 1 and 3");
 
-if (randomNumber === userGuess) {
-  alert("you got it right");
-} else {
-  alert("Try again");
-}
-alert("The random number was: " + randomNumber);
+// if (randomNumber === userGuess) {
+//     alert("you got it right");
+// } else {
+//     alert("Try again");
+// }
+// alert("The random number was: " + randomNumber);
