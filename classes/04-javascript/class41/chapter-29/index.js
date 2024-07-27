0dd4aafdaf8ee3f1c6e7c5c5e3d691c6fc1ad003
alammehtab <mehtaba728@gmail.com>
2024-07-27 10:44:06 +0500

    update chapter29

--- classes/04-javascript/class41/chapter-29/index.js
+++ classes/04-javascript/class41/chapter-29/index.js
@@ -2,26 +2,29 @@
 
 // CONVERTING STRING TO NUMBER
 // number with string type
-// let numString = "25";
-
+// var numString = "25";
 // checking type using typeof operator
 // console.log(typeof numString);
+// example
+// var result = numString + 11
+// console.log(result)
 
 // converting string to number
-// let convertedNumber = Number(numString);
-
+// var convertedNumber = Number(numString);
 // checking type using typeof operator
 // console.log(typeof convertedNumber);
+// example
+// var result = convertedNumber + 11
+// console.log(result)
 
 // CONVERTING NUMBER TO STRING
-
-let hamaraNumber = 35;
+// var hamaraNumber = 35;
 
 // checking type using typeof opertor
 // console.log(typeof hamaraNumber);
 
 // converting number to string
-let convertedString = hamaraNumber.toString();
+// var convertedString = hamaraNumber.toString();
 
 // cheking type using typeof operator
-console.log(typeof convertedString);
+// console.log(typeof convertedString);
