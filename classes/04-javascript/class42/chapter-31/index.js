a64f1f13c2b28571fdf55ad4dc27757b0753a597
alammehtab <mehtaba728@gmail.com>
2024-07-28 10:58:42 +0500

    update chapter42

--- classes/04-javascript/class42/chapter-31/index.js
+++ classes/04-javascript/class42/chapter-31/index.js
@@ -1,7 +1,7 @@
 // CHAPTE-31: GETTING THE CURRENT DATE AND TIME
 
 // GETTING CURRENT DATE AND TIME, DATE OBJECT
-const rightNow = new Date();
+// var rightNow = new Date();
 
 // EXPLAIN THE DATE OBJECT
 // console.log(rightNow);
@@ -9,45 +9,45 @@ const rightNow = new Date();
 // GET DIFFERENT CHUNKS FROM DATE OBJECT
 
 // getting current time in milliseconds
-// const currentTime = rightNow.getTime();
+// var currentTime = rightNow.getTime();
 // console.log(currentTime);
 
 // getting current full year
-// const currentYear = rightNow.getFullYear();
+// var currentYear = rightNow.getFullYear();
 // console.log(currentYear);
 
 // getting current date
-// const currentDate = rightNow.getDate();
+// var currentDate = rightNow.getDate();
 // console.log(currentDate);
 
 // getting current month
-// const currentMonth = rightNow.getMonth();
+// var currentMonth = rightNow.getMonth();
 // console.log(currentMonth);
 
 // getting current day
-// const currentDay = rightNow.getDay();
+// var currentDay = rightNow.getDay();
 // console.log(currentDay);
 
 // CONVERT DATE OBJECT TO STRING USING TOSTRING
 // console.log(typeof rightNow);
-// const dateString = rightNow.toString();
+// var dateString = rightNow.toString();
 // console.log(typeof dateString);
 
 // GET CURRENT DAY NUMBER
-// const currentDay = rightNow.getDay();
+// var currentDay = rightNow.getDay();
 // console.log(rightNow);
 // console.log(currentDay);
 
 // GET TODAY NAME
-const daysNames = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-const currentDayNumber = rightNow.getDay();
-const currentDayName = daysNames[currentDayNumber];
-console.log(currentDayName);
+// var daysNames = [
+//     "Sunday",
+//     "Monday",
+//     "Tuesday",
+//     "Wednesday",
+//     "Thursday",
+//     "Friday",
+//     "Saturday",
+// ];
+// var currentDayNumber = rightNow.getDay();
+// var currentDayName = daysNames[currentDayNumber];
+// console.log(currentDayName);
