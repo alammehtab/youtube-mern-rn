28ca5e8626b93b77efa551bc5f6105a5ab237944
alammehtab <mehtaba728@gmail.com>
2024-07-31 11:07:05 +0500

    update chapter50

--- classes/04-javascript/class54/50/50_index.js
+++ classes/04-javascript/class54/50/50_index.js
@@ -1,11 +1,22 @@
-function setCityName() {
+function setAreaName() {
   var zipCode = +document.getElementById("zipcode").value;
-  var sheher = document.getElementById("sheher");
+  var area = document.getElementById("area");
 
-  if (zipCode === 25180) {
-    sheher.value = "Afghan Colony";
-  } else if (zipCode === 24731) {
-    sheher.value = "Ali Masjid";
+  // we've chosen different areas in Islamabad
+  // 45200 -> Golra
+  // 44210 -> Islamabad F-7 Markaz
+  // 44320 -> Islamabad Federal Board
+  // 44310 -> Islamabad Allama Iqbal Open University
+  // *     -> Invalid zipcode
+
+  if (zipCode === 45200) {
+    area.value = "Golra";
+  } else if (zipCode === 44210) {
+    area.value = "Islamabad F-7 Markaz";
+  } else if (zipCode === 44320) {
+    area.value = "Islamabad Federal Board"
+  } else if (zipCode === 44310) {
+    area.value = "Islamabad Allama Iqbal Open University"
   } else {
     alert("Invalid zipcode");
   }
