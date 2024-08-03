16bafb43b2cbf63cfeec0ade7010cd3025158cd6
alammehtab <mehtaba728@gmail.com>
2024-08-03 10:41:31 +0500

    update js chapter34

--- classes/04-javascript/class43/chapter-34/index.js
+++ classes/04-javascript/class43/chapter-34/index.js
@@ -1,25 +1,25 @@
 // Chapter-34: CHANGING ELEMENTS OF A DATE AND TIME
 
 // OLD STUFF
-// const now = new Date();
+// var now = new Date();
 // console.log(now.getDay());
 
 // SET FULL YEAR
-const now = new Date();
+// var now = new Date();
 // console.log({ now });
 // console.log("before setting: ", now.getFullYear());
 
-// now.setFullYear("2035");
+// setting full year
+// now.setFullYear(2035);
 // console.log({ now });
 // console.log("after setting: ", now.getFullYear());
 
 // SET HOURS
-
 // before setting
-console.log("before setting hours: ", now.getHours());
+// console.log("before setting hours: ", now.getHours());
 
 // setting
-now.setHours(22);
+// now.setHours(22);
 
 // after setting
-console.log("after setting hours: ", now.getHours());
+// console.log("after setting hours: ", now.getHours());
