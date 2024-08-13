a5614fd3cd01ca276b08db6328ec83bd6f50ac57
alammehtab <mehtaba728@gmail.com>
2024-08-13 11:22:29 +0500

    update js ch55

--- classes/04-javascript/class55/55/index.js
+++ classes/04-javascript/class55/55/index.js
@@ -1,5 +1,18 @@
+var text = document.getElementById('text');
+
+// Set initial styles explicitly
+text.style.color = 'black';
+text.style.fontSize = '20px';
+text.style.border = '1px solid black';
+
 function changeColor() {
-  const hamaraPara = document.getElementById("greetings");
-  hamaraPara.style.color = "red";
-  hamaraPara.style.fontWeight = "bolder";
+  text.style.color = text.style.color === 'black' ? 'red' : 'black';
+}
+
+function changeFontSize() {
+  text.style.fontSize = text.style.fontSize === '20px' ? '30px' : '20px';
+}
+
+function toggleBorder() {
+  text.style.border = text.style.border === '1px solid black' ? '3px dashed blue' : '1px solid black';
 }
