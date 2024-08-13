807826253fb27ef00dc57d25642a9ce0fddfb9d3
alammehtab <mehtaba728@gmail.com>
2024-08-13 08:45:06 +0500

    update js ch54

--- classes/04-javascript/class55/54/index.js
+++ classes/04-javascript/class55/54/index.js
@@ -1,5 +1,15 @@
-function enlargeImage() {
-  const imageToSwap = document.getElementById("imageToSwap");
-  imageToSwap.className = "biggerImage";
-  imageToSwap.src = "./images/car.webp";
+function swapImage(newImageSrc, clickedThumbnail) {
+  var mainImage = document.getElementById("mainImage");
+  // swap images
+  mainImage.src = newImageSrc;
+
+  // Remove 'active' class from all thumbnails
+  var thumbnails = document.querySelectorAll(".gallery img");
+  // thumbnails.forEach(img => img.classList.remove("active"));
+  for (var i = 0; i < thumbnails.length; i++) {
+    thumbnails[i].classList.remove("active")
+  }
+
+  // Add 'active' class to the clicked thumbnail
+  clickedThumbnail.classList.add("active");
 }
