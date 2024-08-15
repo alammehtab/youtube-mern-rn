668574e1851d45b322e9a4dc20ab9248b5b4c524
alammehtab <mehtaba728@gmail.com>
2024-08-15 15:15:08 +0500

    update js ch60

--- classes/04-javascript/class56/chapter-60/example0-children.js
+++ classes/04-javascript/class56/chapter-60/example0-children.js
@@ -2,11 +2,11 @@
 var parentDiv = document.getElementById('parentDiv');
 
 // Get all child elements
-var childElements = parentDiv.children;
+var childElements = parentDiv.children; // children return only html elements
 
-console.log('All Child Elements:', childElements);
+// console.log('All Child Elements:', childElements);
 
 // Loop through child elements and change their text color
-for (var i = 0; i < childElements.length; i++) {
-    childElements[i].style.color = 'blue';
-}
+// for (var i = 0; i < childElements.length; i++) {
+//     childElements[i].style.color = 'blue';
+// }
