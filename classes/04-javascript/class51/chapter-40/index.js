8240076b1f75b1aaa8f157c7320226bcb92fd1d9
alammehtab <mehtaba728@gmail.com>
2024-08-24 10:01:40 +0500

    update js ch40

--- classes/04-javascript/class51/chapter-40/index.js
+++ classes/04-javascript/class51/chapter-40/index.js
@@ -7,31 +7,31 @@
 // syntax- first 3 lines
 
 // UNDERSTANDING THE WHOLE PROGRAM
-const day = "Friday"
+// var day = "Sunday"
 
-switch (day) {
-    case "Monday":
-        console.log("Today is Monday")
-        break
-    case "Tuesday":
-        console.log("Today is Tuesday")
-        break
-    case "Wednesday":
-        console.log("Today is Wednesday")
-        break
-    case "Thurday":
-        console.log("Today is Thurday")
-        break
-    case "Friday":
-        console.log("Today is Friday")
-        console.log("BLESSED FRIDAY")
-        break
-    case "Saturday":
-        console.log("Today is Saturday")
-        break
-    case "Sunday":
-        console.log("Today is Sunday")
-        break
-    default:
-        console.log("Invalid day")
-}
+// switch (day) {
+//     case "Sunday":
+//         console.log("Today is Sunday")
+//         break
+//     case "Monday":
+//         console.log("Today is Monday")
+//         break
+//     case "Tuesday":
+//         console.log("Today is Tuesday")
+//         break
+//     case "Wednesday":
+//         console.log("Today is Wednesday")
+//         break
+//     case "Thurday":
+//         console.log("Today is Thurday")
+//         break
+//     case "Friday":
+//         console.log("Today is Friday")
+//         console.log("BLESSED FRIDAY")
+//         break
+//     case "Saturday":
+//         console.log("Today is Saturday")
+//         break
+//     default:
+//         console.log("Invalid day")
+// }
