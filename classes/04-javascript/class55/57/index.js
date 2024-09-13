93fadfe4bd6adf33110ebaf9c1a8edf3006029dd
alammehtab <mehtaba728@gmail.com>
2024-09-13 19:27:14 +0500

    update js ch57

--- classes/04-javascript/class55/57/index.js
+++ classes/04-javascript/class55/57/index.js
@@ -1,14 +1,23 @@
 // Get all <li> elements
 const listItems = document.getElementsByTagName('li');
 
-// Highlight even items
-document.getElementById('highlightEven').onclick = function () {
+// Highlight even items (applying onclick on each element)
+// document.getElementById('highlightEven').onclick = function () {
+//   for (let i = 0; i < listItems.length; i++) {
+//     if ((i + 1) % 2 === 0) { // even 
+//       listItems[i].classList.add('highlight');
+//     }
+//   }
+// };
+
+// Highlight even items (using addEvenetListener)
+document.getElementById('highlightEven').addEventListener('click', function () {
   for (let i = 0; i < listItems.length; i++) {
     if ((i + 1) % 2 === 0) { // even 
       listItems[i].classList.add('highlight');
     }
   }
-};
+})
 
 // Highlight odd items
 document.getElementById('highlightOdd').onclick = function () {
