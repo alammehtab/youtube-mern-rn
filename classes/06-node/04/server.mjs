708b694b0b03f8786dbafa054e148d3d1620253c
alammehtab <mehtaba728@gmail.com>
2024-09-14 13:12:04 +0500

    add node 04

---
+++ classes/06-node/04/server.mjs
@@ -0,0 +1,35 @@
+import { createServer } from 'node:http'
+
+const PORT = 3000
+
+const server = createServer((req, res) => {
+    // picking up stuff from req object
+    // console.log({ method: req.method, url: req.url })
+
+    // destructure stuff from req
+    const { method, url } = req;
+    const parsedUrl = new URL(url, `http://${req.headers.host}`);
+
+    // Set response header to JSON
+    res.setHeader('Content-Type', 'application/json');
+
+    if (method === 'GET' && parsedUrl.pathname === '/api/items') {
+        res.statusCode = 200;
+        res.end(JSON.stringify({ message: 'GET request - Fetching all items' }));
+
+        // POST Request
+    } else if (method === 'POST' && parsedUrl.pathname === '/api/items') {
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+        req.on('end', () => {
+            const newItem = JSON.parse(body);
+            res.statusCode = 201; //created
+            res.end(JSON.stringify({ message: `POST request - Adding new item`, data: newItem }));
+        });
+        // PUT Request
+    }
+})
+
+server.listen(PORT, () => console.log(`Server is listening here: http://localhost:${PORT}`))
