e797980e2728b84104cc755ce10d31e01b1fb9a6
alammehtab <mehtaba728@gmail.com>
2024-09-14 17:49:50 +0500

    remove commented code

--- classes/04-javascript/class55/57/index.js
+++ classes/04-javascript/class55/57/index.js
@@ -1,15 +1,6 @@
 // Get all <li> elements
 const listItems = document.getElementsByTagName('li');
 
-// Highlight even items (applying onclick on each element)
-// document.getElementById('highlightEven').onclick = function () {
-//   for (let i = 0; i < listItems.length; i++) {
-//     if ((i + 1) % 2 === 0) { // even 
-//       listItems[i].classList.add('highlight');
-//     }
-//   }
-// };
-
 // Highlight even items (using addEvenetListener)
 document.getElementById('highlightEven').addEventListener('click', function () {
   for (let i = 0; i < listItems.length; i++) {
