829bd2ab33f66abd0d18ff14c7e7a794eb6a942b
alammehtab <mehtaba728@gmail.com>
2024-10-08 21:43:24 +0500

    update js ch76

--- classes/04-javascript/class60/chapter-76/index.js
+++ classes/04-javascript/class60/chapter-76/index.js
@@ -1,14 +1,1 @@
-// Chapter-76: Browser control: Getting and setting the URL
 
-// GET URL
-const meKahaHu = window.location.href;
-
-// GET DIFFERENT PARTS OF URL
-
-// domain name
-const domainName = window.location.hostname;
-
-// path
-const path = window.location.pathname;
-
-console.log(window.location);
