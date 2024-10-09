6d02cbaf0f7d56063fb4f7d47a81ea536e510d21
alammehtab <mehtaba728@gmail.com>
2024-10-09 19:01:35 +0500

    update js ch77

--- classes/04-javascript/class61/chapter-77/index.js
+++ classes/04-javascript/class61/chapter-77/index.js
@@ -1,15 +1,1 @@
 // Chapter-77: Browser control: Getting and setting the URL another way
-
-// get url
-// const href = window.location.href;
-
-// set url
-// window.location.href = "https://www.w3schools.com/";
-
-// set url using assign
-// window.location.assign("https://www.w3schools.com/");
-
-// browser reload button
-function reloadThePage() {
-  window.location.reload();
-}
