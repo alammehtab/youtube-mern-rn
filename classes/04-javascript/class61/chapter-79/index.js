2091f6ae340375bc0e1364d9b93ef087658c03b3
alammehtab <mehtaba728@gmail.com>
2024-10-09 19:17:50 +0500

    update js ch79

--- classes/04-javascript/class61/chapter-79/index.js
+++ classes/04-javascript/class61/chapter-79/index.js
@@ -1,14 +1,2 @@
 // Chapter-79: Browser control: Filling the window with content
-function openGoogle() {
-  window.open("https://www.google.com.pk");
-}
 
-function readTheStory() {
-  // const newWindow = window.open();
-  // newWindow.location.href = "http://127.0.0.1:5500/chapter-79/story.html";
-
-  window.open("http://127.0.0.1:5500/chapter-79/story.html");
-}
-
-// close a window
-// window.close();
