1e22f3873a8c0e7cde245917af7e31327e41751e
alammehtab <mehtaba728@gmail.com>
2024-10-09 20:36:48 +0500

    update js ch76

--- classes/04-javascript/class60/chapter-76/index.js
+++ classes/04-javascript/class60/chapter-76/index.js
@@ -1,1 +1,22 @@
+// Chapter 76: Browser Control - Getting and Setting the URL
 
+// GET FULL URL OF THE CURRENT PAGE
+// var currentURL = window.location.href;
+// console.log(currentURL);
+
+// GET DOMAIN NAME
+// var domain = window.location.hostname;
+// console.log({ domain })
+
+// GET PATH NAME
+// var pathname = window.location.pathname;
+// console.log({pathname})
+
+// GET ANCHOR/HASH (DIFFERENT SECTIONS ON PAGE, CURRENT SECTION)
+// NOTE: for this u can visit https://mehtab.netlify.app/#about
+// var anchor = window.location.hash; 
+// console.log({ anchor })
+
+// SET NEW URL
+// window.location.href = 'https://www.google.com/'
+// window.location.href = 'https://mehtab.netlify.app'
