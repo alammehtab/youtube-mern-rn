507bac300cd6e5cfebc014eaf4c3d6e5b567c313
alammehtab <mehtaba728@gmail.com>
2024-10-11 13:57:45 +0500

    finalize js ch78

--- classes/04-javascript/class61/chapter-78/index.js
+++ classes/04-javascript/class61/chapter-78/index.js
@@ -1,1 +1,17 @@
 // Chapter - 78: Browser control: Forward and reverse
+
+// GO BACK IN HISTORY, ONE PAGE/STEP, PREVIOUS PAGE
+// history.back()
+
+// GO FORWARD IN HISTORY
+// history.forward()
+
+// GO FORWARD IN HISTORY MULTIPLE STEPS/PAGES
+// history.go(2)
+
+// GO BACK IN HISTORY MULTIPLE STEPS/PAGES
+// history.go(-2)
+
+// CHECK REFERRER
+// var whereUserCameFrom = document.referrer
+// console.log({ whereUserCameFrom })
