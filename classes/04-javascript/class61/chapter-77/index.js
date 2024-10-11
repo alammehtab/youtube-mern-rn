0c9122d615490583796ffecb0e82bcbbf17ec0c3
alammehtab <mehtaba728@gmail.com>
2024-10-11 12:21:15 +0500

    finalize ch77

--- classes/04-javascript/class61/chapter-77/index.js
+++ classes/04-javascript/class61/chapter-77/index.js
@@ -1,1 +1,12 @@
 // Chapter-77: Browser control: Getting and setting the URL another way
+
+// ASSIGN METHOD (can go back, page is stored in the history)
+// window.location.assign('https://www.youtube.com')
+
+// REPLACE METHOD (can't go back, page isn't stored in the history)
+// window.location.replace('http://www.google.com')
+
+// RELOAD CURRENT PAGE 
+// window.location.reload(true); // Reloads from server
+// window.location.reload(false); // Reloads from cache
+// window.location.reload(); // Default: cache
