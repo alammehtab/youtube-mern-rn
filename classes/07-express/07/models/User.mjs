7e047ef362ae9dcde35653bd55681f2a48c83a7d
alammehtab <mehtaba728@gmail.com>
2024-10-13 11:57:45 +0500

    add class7

---
+++ classes/07-express/07/models/User.mjs
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+}, {
+    timestamps: true
+});
+
+export const User = mongoose.model('User', userSchema);
