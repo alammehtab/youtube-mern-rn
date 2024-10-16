126f2731e0526753a930058a5c544af9699c7dcd
alammehtab <mehtaba728@gmail.com>
2024-10-16 21:55:39 +0500

    finalize js ch80

--- classes/04-javascript/class61/chapter-80/index.js
+++ classes/04-javascript/class61/chapter-80/index.js
@@ -1,10 +1,22 @@
 // Chapter-80: Browser control: Controlling the window's size and location
 
-// open new window with a name
-function openNewWindowWithName() {
-  window.open(
-    "http://127.0.0.1:5500/chapter-80/story.html",
-    "Test",
-    "width=420,height=380,left=200,top=100"
-  );
-}
+// 1. OPENING A POPUP WITH A NAME
+
+// without name
+// var monkeyWindow = window.open()
+
+// fill url
+// var monkeyWindow = window.open("test.html")
+
+// provide url and name both
+// var monkeyWindow = window.open("test.html", "Name from Script")
+
+// 2. CONTROLLING THE WINDOWS SIZE
+// window.open("test.html", " ", "width=200,height=200")
+
+// 3. POSITIONING THE WINDOW ON THE SCREEN
+// window.open("test.html", " ", "width=250,height=250,top=400,left=500")
+
+// 4. COMBINING SIZE AND POSITION PARAMETERS IN A VARIABLE
+// var windowSpecs = "width=250,height=250,top=400,left=500"
+// window.open("test.html", " ", windowSpecs)
