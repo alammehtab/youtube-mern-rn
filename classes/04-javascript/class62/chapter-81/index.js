c8c9b454d6c9f231a5d860e011a2782760bef755
alammehtab <mehtaba728@gmail.com>
2024-10-18 10:01:01 +0500

    finalize js ch81

--- classes/04-javascript/class62/chapter-81/index.js
+++ classes/04-javascript/class62/chapter-81/index.js
@@ -1,1 +1,21 @@
 // Chapter 81: Browser Control - Testing for Popup Blockers
+
+// TEST FOR POPUP BLOCKER
+// var testPop = window.open("", "", "width=100,height=100");
+// if (testPop === null) {
+//     alert("Please disable your popup blocker.");
+// } else {
+//     // alert("Popup blocker is disabled on your computer.")
+//     testPop.close();
+// }
+
+// convert above code to function, call it onload in html body
+function checkForPopBlocker() {
+    var testPop = window.open("", "", "width=100,height=100");
+    if (testPop === null || typeof (testPop) === "undefined") {
+        alert("Please disable your popup blocker.");
+    } else {
+        // alert("Popup blocker is disabled on your computer.")
+        testPop.close();
+    }
+}
