afdbc9a7ee55d94a9d469341710c023dc32c762d
alammehtab <mehtaba728@gmail.com>
2024-10-18 21:26:52 +0500

    finalize js ch83

--- classes/04-javascript/class62/chapter-83/index.js
+++ classes/04-javascript/class62/chapter-83/index.js
@@ -1,2 +1,21 @@
 // Chapter - 83: Form validation: drop - downs
 
+// VALIDATION FUNCTION
+// function checkForSelection(selecID) {
+//     var target = document.getElementById(selecID);
+//     if (target.selectedIndex === 0) {
+//         alert("Please select a state.");
+//         return false; // Prevents form submission
+//     }
+// }
+
+// IMPROVING USABLITY
+
+// function checkForSelection(selecID) {
+//     var target = document.getElementById(selecID);
+//     if (target.selectedIndex === 0) {
+//         alert("Please select a state.");
+//         target.focus(); // Move focus back to the drop-down
+//         return false; // Prevents form submission
+//     }
+// }
