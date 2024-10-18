c4e3038c97bafbc24464a5d92b57c76e5c25fd67
alammehtab <mehtaba728@gmail.com>
2024-10-18 21:50:47 +0500

    finalize js ch84

--- classes/04-javascript/class62/chapter-84/index.js
+++ classes/04-javascript/class62/chapter-84/index.js
@@ -1,1 +1,13 @@
 // Chapter-84: Form validation: radio-buttons
+
+// VALIDATION FUNCTION
+// function validateRadios(groupName) {
+//     var radios = document.getElementsByName(groupName);
+//     for (var i = 0; i < radios.length; i++) {
+//         if (radios[i].checked) {
+//             return true; // At least one option is selected
+//         }
+//     }
+//     alert("Please check one.");
+//     return false; // Prevents form submission
+// }
