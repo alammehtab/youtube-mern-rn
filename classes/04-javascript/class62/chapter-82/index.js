d157239a9aadd90d4e9467ab14188cafca13b0bc
alammehtab <mehtaba728@gmail.com>
2024-10-18 20:40:15 +0500

    finalize js ch82

--- classes/04-javascript/class62/chapter-82/index.js
+++ classes/04-javascript/class62/chapter-82/index.js
@@ -1,1 +1,32 @@
 // Chapter 82: Form Validation - Text Fields
+
+// FUNCTION FOR VALIDATION
+// function checkForLastName() {
+//     if (document.getElementById("lastNameField").value.length === 0) {
+//         alert("Please enter your last name");
+//         return false; // Stops form submission
+//     }
+// }
+
+// IMPROVED FUNCTION WITH FOCUS() IN FIELD AFTER INVALID SUBMISSION
+// function checkForLastName() {
+//     var targetField = document.getElementById("lastNameField");
+//     if (targetField.value.length === 0) {
+//         alert("Please enter your last name");
+//         targetField.focus(); // Places cursor in the field
+//         return false;
+//     }
+// } 
+
+// ADDING VISUAL FEEDBACK
+
+// function checkForLastName() {
+//     var targetField = document.getElementById("lastNameField");
+//     if (targetField.value.length === 0) {
+//         alert("Please enter your last name");
+//         targetField.style.background = "yellow"; // Highlights empty field
+//         targetField.focus();
+//         return false;
+//     }
+//     targetField.style.background = "white"; // Restores background after valid input
+// }
