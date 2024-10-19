100712191dfcf461fbf876ffd04263c43613a315
alammehtab <mehtaba728@gmail.com>
2024-10-19 15:27:29 +0500

    update js ch88

--- classes/04-javascript/class62/chapter-88/index.js
+++ classes/04-javascript/class62/chapter-88/index.js
@@ -1,23 +1,2 @@
-function checkPassword() {
-  try {
-    let pass = document.getElementById("password").value;
-    if (pass.length < 8) {
-      throw "Please enter at least 8 characters.";
-    }
-    if (pass.indexOf(" ") !== -1) {
-      throw "No spaces in the password, please.";
-    }
-    let numberSomewhere = false;
-    for (let i = 0; i < pass.length; i++) {
-      if (isNaN(pass[(i, i + 1)]) === false) {
-        numberSomewhere = true;
-        break;
-      }
-    }
-    if (numberSomewhere === false) {
-      throw "Include at least 1 number.";
-    }
-  } catch (err) {
-    alert(err);
-  }
-}
+// Chapter - 88: Exceptions: throw
+
