a461520b84268c0c2dbed4654048f7d7136f148f
alammehtab <mehtaba728@gmail.com>
2024-11-09 18:40:05 +0500

    finalize js ch85

--- classes/04-javascript/class62/chapter-85/index.js
+++ classes/04-javascript/class62/chapter-85/index.js
@@ -1,26 +1,26 @@
 // Chapter - 85: Form validation: ZIP codes
 
-// VALIDATION FUNCTION
+// VALIDATION FUNCTION FOR ZIP CODE
+function validateZIP() {
+    // get zip code value, user ne jo zipcode value enter ki h
+    var zipcode = document.getElementById("zip").value
 
-// function validateZIP() {
-//     var valueEntered = document.getElementById("zip").value;
-//     var numChars = valueEntered.length;
+    // Check if ZIP code has fewer than 5 digits
+    if (zipcode.length < 5) {
+        alert("Please enter a 5-digit code.")
+        return false // form submit hone se rokdo
+    }
 
-//     // Check if ZIP code has fewer than 5 digits
-//     if (numChars < 5) {
-//         alert("Please enter a 5-digit code.");
-//         return false; // ta k form submit na ho
-//     }
+    // Loop through each character to ensure it is a number
+    for (var i = 0; i < 5; i++) {
+        var currentCharacter = parseInt(zipcode[i])
 
-//     // Loop through each character to ensure it is a number
-//     for (var i = 0; i < 5; i++) {
-//         var thisChar = parseInt(valueEntered[i]);
-//         if (isNaN(thisChar)) {
-//             alert("Please enter only numbers.");
-//             return false; // Cancel form submission if a non-numeric character is found
-//         }
-//     }
-//     return true; // ZIP code is valid, allow form submission
-// }
+        // agr ye character number na ho
+        if (isNaN(currentCharacter)) {
+            alert("Please enter only numbers.");
+            return false;
+        }
+    }
 
-
+    return true
+}
