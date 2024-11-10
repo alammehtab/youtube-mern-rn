8d1a346ed0110cc874596522361e85c11a3caf65
alammehtab <mehtaba728@gmail.com>
2024-11-10 11:56:47 +0500

    finalize js ch89

--- classes/04-javascript/class62/chapter-89/index.js
+++ classes/04-javascript/class62/chapter-89/index.js
@@ -1,1 +1,14 @@
 // Chapter - 89: Handling events within JavaScript
+
+function sayHello() {
+    alert("Hellooooo")
+}
+
+// get the button
+// var ourButton = document.getElementById("button1")
+
+// // add event listener to it
+// ourButton.onclick = sayHello
+
+// short form
+// document.getElementById("button1").onclick = sayHello
