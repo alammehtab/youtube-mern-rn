4cc5f21d8ca3ce4fa0139659320316692dfdcbfd
alammehtab <mehtaba728@gmail.com>
2024-11-10 10:13:31 +0500

    finalize js ch87

--- classes/04-javascript/class62/chapter-87/index.js
+++ classes/04-javascript/class62/chapter-87/index.js
@@ -1,2 +1,24 @@
 // Chapter-87: Exceptions: try and catch
 
+// VALID CODE
+// function greetTheWorld() {
+//     var greetings = "Hello world!"
+//     alert(greetings)
+// }
+
+// 1. Identifying an Error
+// function greetTheWorld() {
+//     var greetings = "Hello world!"
+//     aler(greetings)
+// }
+
+// 2. Using try and catch
+// function greetTheWorld() {
+//     try {
+//         // whatever you want to do
+//         var greeting = "Hello world!";
+//         aler(greeting);  // Mistake: 'aler' is not a function
+//     } catch (error) {
+//         alert(error) // display error message
+//     }
+// }
